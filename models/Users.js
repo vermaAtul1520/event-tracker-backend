be72c89e6baf9e28ae1a2919b4b870c717c0bf7f
vermaAtul1520 <atulverma1520@gmail.com>
2024-01-30 00:47:22 +0530

    user schema added

---
+++ models/Users.js
@@ -0,0 +1,34 @@
+// models/User.js
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+});
+
+// Hash the user password before saving it to the database
+// userSchema.pre('save', async function (next) {
+//   try {
+//     const salt = await bcrypt.genSalt(10);
+//     const hashedPassword = await bcrypt.hash(this.password, salt);
+//     this.password = hashedPassword;
+//     next();
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+// // Verify user password
+// userSchema.methods.isValidPassword = async function (password) {
+//   try {
+//     return await bcrypt.compare(password, this.password);
+//   } catch (error) {
+//     throw new Error(error);
+//   }
+// };
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
