34935a68416f394f6710a4ae5df23ec2295011fc
vermaAtul1520 <atulverma1520@gmail.com>
2024-01-30 00:47:53 +0530

    ticket schema added

---
+++ models/Ticket.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const ticketSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  event: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
+  ticketType: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+});
+
+const Ticket = mongoose.model('Ticket', ticketSchema);
+
+module.exports = Ticket;
